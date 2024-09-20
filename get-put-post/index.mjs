5a6585bb480809041cab74076fbd235a87f538f9
maryamr25 <maryamrasheed0987@gmail.com>
2024-09-20 19:32:55 +0500

    add the get request

--- get-put-post/index.mjs
+++ get-put-post/index.mjs
@@ -17,41 +17,41 @@ const server = createServer((req, res) => {
         res.statusCode = 200;
         res.end(JSON.stringify({ message: 'GET request - Fetching all items' }));
 
-        // POST Request
-    } else if (method === 'POST' && parsedUrl.pathname === '/api/items') {
-        let body = '';
-        req.on('data', chunk => {
-            body += chunk.toString();
-        });
-        req.on('end', () => {
-            const newItem = JSON.parse(body);
-            res.statusCode = 201; //created
-            res.end(JSON.stringify({ message: `POST request - Adding new item`, data: newItem }));
-        });
-        // PUT Request
-    }else if (method === 'PUT' && parsedUrl.pathname.startsWith('/api/items/')) {
-        let body = '';
-        const itemId = parsedUrl.pathname.split('/').pop();
-        req.on('data', chunk => {
-          body += chunk.toString();
-        });
-        req.on('end', () => {
-          const updatedItem = JSON.parse(body);
-          res.statusCode = 200;
-          res.end(JSON.stringify({ message: `PUT request - Updating item ${itemId}`, data: updatedItem }));
-        });
+//         // POST Request
+//     } else if (method === 'POST' && parsedUrl.pathname === '/api/items') {
+//         let body = '';
+//         req.on('data', chunk => {
+//             body += chunk.toString();
+//         });
+//         req.on('end', () => {
+//             const newItem = JSON.parse(body);
+//             res.statusCode = 201; //created
+//             res.end(JSON.stringify({ message: `POST request - Adding new item`, data: newItem }));
+//         });
+//         // PUT Request
+//     }else if (method === 'PUT' && parsedUrl.pathname.startsWith('/api/items/')) {
+//         let body = '';
+//         const itemId = parsedUrl.pathname.split('/').pop();
+//         req.on('data', chunk => {
+//           body += chunk.toString();
+//         });
+//         req.on('end', () => {
+//           const updatedItem = JSON.parse(body);
+//           res.statusCode = 200;
+//           res.end(JSON.stringify({ message: `PUT request - Updating item ${itemId}`, data: updatedItem }));
+//         });
     
-        // DELETE Request
-        } else if (method === 'DELETE' && parsedUrl.pathname.startsWith('/api/items/')) {
-            const itemId = parsedUrl.pathname.split('/').pop();
-            res.statusCode = 200;
-            res.end(JSON.stringify({ message: `DELETE request - Deleting item ${itemId}` }));
+//         // DELETE Request
+//         } else if (method === 'DELETE' && parsedUrl.pathname.startsWith('/api/items/')) {
+//             const itemId = parsedUrl.pathname.split('/').pop();
+//             res.statusCode = 200;
+//             res.end(JSON.stringify({ message: `DELETE request - Deleting item ${itemId}` }));
     
-        // Handle 404 Not Found
-        } else {
-            res.statusCode = 404;
-            res.end(JSON.stringify({ message: 'Route not found' }));
-        }
-})
+//         // Handle 404 Not Found
+//         } else {
+//             res.statusCode = 404;
+//             res.end(JSON.stringify({ message: 'Route not found' }));
+//         }
+// })
 
-server.listen(PORT, () => console.log(`Server is listening here: http://localhost:${PORT}`))
+// server.listen(PORT, () => console.log(`Server is listening here: http://localhost:${PORT}`))
