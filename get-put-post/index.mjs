d55b2d45f65b7afa2a27030a04ca8ecaeebc0aa6
maryamr25 <maryamrasheed0987@gmail.com>
2024-09-20 19:38:05 +0500

    add put reequest

--- get-put-post/index.mjs
+++ get-put-post/index.mjs
@@ -41,17 +41,17 @@ const server = createServer((req, res) => {
           res.end(JSON.stringify({ message: `PUT request - Updating item ${itemId}`, data: updatedItem }));
         });
     
-        // DELETE Request
-        } else if (method === 'DELETE' && parsedUrl.pathname.startsWith('/api/items/')) {
-            const itemId = parsedUrl.pathname.split('/').pop();
-            res.statusCode = 200;
-            res.end(JSON.stringify({ message: `DELETE request - Deleting item ${itemId}` }));
+//         // DELETE Request
+//         } else if (method === 'DELETE' && parsedUrl.pathname.startsWith('/api/items/')) {
+//             const itemId = parsedUrl.pathname.split('/').pop();
+//             res.statusCode = 200;
+//             res.end(JSON.stringify({ message: `DELETE request - Deleting item ${itemId}` }));
     
-        // Handle 404 Not Found
-        } else {
-            res.statusCode = 404;
-            res.end(JSON.stringify({ message: 'Route not found' }));
-        }
-})
+//         // Handle 404 Not Found
+//         } else {
+//             res.statusCode = 404;
+//             res.end(JSON.stringify({ message: 'Route not found' }));
+//         }
+// })
 
-server.listen(PORT, () => console.log(`Server is listening here: http://localhost:${PORT}`))
+// server.listen(PORT, () => console.log(`Server is listening here: http://localhost:${PORT}`))
